8784d6ce10da70a02c90a9c3b027c4e77b2e82e2
Ajit Jadhav <ajitjadhav2310@outlook.com>
2020-07-05 21:21:17 +0530

    queries added

--- apis/routes/Project/index.js
+++ apis/routes/Project/index.js
@@ -7,126 +7,88 @@ const constants = require('./../../../Constants')
 const Project = require('./../../Models/Project')
 const Task = require('./../../Models/Task')
 const utilities = require('./../Utilities')
+require('./../Utilities')
 require('dotenv').config()
 const DB_HOST = 'mongodb://localhost:27017'
 
 /* ------------------------------- GET REQUESTS ------------------------------- */
 
 
-// Get all the projects
-ROUTER.get('/', (req, res, error) => {
-    var myProjects = []
-    mongo.connect(DB_HOST, (error, dbClient) => {
-        if (error) {
-            let err = new Error(error)
-            err['status'] = constants.statusCodes.badRequest
-            next(err)
-        }
-        else {
-            const database = dbClient.db('colabnova')
-            var dbCursor = database.collection('projects').find()
-            dbCursor.forEach((project, error) => {
-                if (error) {
-                    next(error)
-                }
-                else {
-                    myProjects.push(project)
-                }
-            }, () => {
-                dbClient.close()
-                res.status(constants.statusCodes.OK).json({
-                    'message': 'success',
-                    'data': myProjects
-                })
-            });
-        }
-    })
-})
-
 // Get all the tasks
 ROUTER.get('/my-tasks', (req, res, next) => {
-    var myTasks = []
-    mongo.connect(process.env.DB_HOST ? process.env.DB_HOST : DB_HOST, (error, dbClient) => {
-        if (error) {
-            let err = new Error(error ? error : 'Error while connecting to host')
-            err['status'] = constants.statusCodes.serverError
-            next(err)
+    if (req.query && req.query.projectId) {
+        const identifiers = {
+            'projectId': req.query.projectId
         }
-        else {
-            const database = dbClient.db(process.env.APP_DATABASE ? process.env.APP_DATABASE : 'colabnova')
-            let dbCursor = database.collection('tasks').find()
-            dbCursor.forEach(task => {
-                myTasks.push(task)
-            }, () => {
-                dbClient.close()
+        utilities.getDocuments('colabnova', 'tasks', identifiers)
+            .then(result => {
                 res.status(constants.statusCodes.OK).json({
-                    'message': 'success',
-                    'data': myTasks
+                    'data': result
                 })
             })
-        }
-    })
-})
-
-
-// Fetch all the project data
-ROUTER.get('/:projectId', (req, res, next) => {
-    const projectId = req.params.projectId
-    if (projectId) {
-        Project
-            .findOne({ '_id': projectId })
-            .exec()
-            .then(document => {
-                console.log('found')
-                res.status(constants.statusCodes.OK).json({
-                    'message': 'found',
-                    'data': document
-                })
-            })
-            .catch(err => {
-                console.log(err)
-                let error = new Error(err)
-                error['status'] = constants.statusCodes.serverError
-                next(error)
+            .catch(error => {
+                let err = new Error(error['message'])
+                err['status'] = constants.statusCodes.badRequest
+                next(err)
             })
     }
     else {
-        let error = new Error('No project id found')
-        error['status'] = constants.statusCodes.noData
-        next(error)
-    }
-})
-
-// Fetch task details
-ROUTER.get('/task/:taskId', (req, res, next) => {
-    const taskId = req.params.taskId
-    if (taskId) {
-        let myTasks = []
-        mongoClient.connect(process.envAPP_DB_HOST, (error, database) => {
-            assert.equal(null, error)
-            if (error) {
-                let err = new Error(error)
-                err['status'] = constants.statusCodes.serverError
-                next(err)
-            }
-            else {
-                var cursor = database.collection('Task').find()
-                cursor.forEach((document, err) => {
-                    assert(null, err)
-                    myTasks.push(document)
-                });
-                res.status(constants.statusCodes.OK).json({
-                    'message': 'success',
-                    'data': myTasks
-                })
-            }
-            database.close
+        res.status(constants.statusCodes.badRequest).json({
+            'message': 'No projectId given'
         })
     }
-    else {
-        let error = new Error('No task id found')
-        error['status'] = constants.statusCodes.noData
-        next(error)
+})
+
+// Get all projects
+ROUTER.get('/my-projects', (req, res, next) => {
+    utilities.getDocuments('colabnova', 'projects')
+        .then(result => {
+            res.status(constants.statusCodes.OK).json({
+                'data': result
+            })
+        })
+        .catch(error => {
+            let err = new Error(error['message'])
+            err['status'] = constants.statusCodes.badRequest
+            next(err)
+        })
+})
+
+// Task details
+ROUTER.get('/task', (req, res, next) => {
+    const identifiers = {
+        'taskId': req.query.taskId
+    }
+    utilities.getDocuments('colabnova', 'tasks', identifiers)
+        .then(result => {
+            res.status(200).json({
+                'data': result
+            })
+        })
+        .catch(error => {
+            res.status(501).json({
+                'data': error
+            })
+        })
+})
+
+// Get project details
+ROUTER.get('/:projectId', (req, res, next) => {
+    if (req.params && req.params.projectId) {
+        const identifiers = {
+            'projectId': req.params.projectId
+        }
+        utilities.getDocuments('colabnova', 'projects', identifiers)
+            .then(result => {
+                res.status(constants.statusCodes.created).json({
+                    'data': result[0]
+                })
+            })
+            .catch(error => {
+                res.status(constants.statusCodes.serverError).json({
+                    'data': error
+                })
+            })
     }
 })
 
@@ -137,37 +99,25 @@ ROUTER.get('/task/:taskId', (req, res, next) => {
 // Create a project
 ROUTER.post('/', (req, res, next) => {
     var project = {
-        'projectId': `${Math.random().toString(36).slice(2)}-${Math.random().toString(36).slice(2)}-${Math.random().toString(36).slice(2)}`,
+        'projectId': `PROJECT_${Math.random().toString(36).slice(2)}-${Math.random().toString(36).slice(2)}-${Math.random().toString(36).slice(2)}`,
         'name': req.body.projectName,
         'owner': req.body.projectOwner,
         'description': req.body.description ? req.body.description : 'No description available',
-        'tasks': []
+        'createdOn': req.body.createdOn
     }
-    mongo.connect('mongodb://localhost:27017', function (err, dbClient) {
-        assert.equal(null, err)
-        if (err) {
-            let error = new Error(err)
-            error['status'] = constants.statusCodes.serverError
-            next(error)
-        }
-        else {
-            const database = dbClient.db('colabnova')
-            database.collection('projects').insertOne(project, (error, result) => {
-                if (error) {
-                    dbClient.close()
-                    let err = new Error(error)
-                    err['status'] = constants.statusCodes.serverError
-                    next(err)
-                }
-                else {
-                    dbClient.close()
-                    res.status(constants.statusCodes.created).json({
-                        'message': 'success'
-                    })
-                }
+    utilities.insertData('colabnova', 'projects', project)
+        .then(result => {
+            res.status(constants.statusCodes.OK).json({
+                'message': 'Project created',
+                'projectId': project['projectId']
             })
-        }
-    })
+        })
+        .catch(error => {
+            res.status(404).json({
+                'message': 'failed',
+                'data': error
+            })
+        })
 })
 
 
@@ -184,48 +134,30 @@ ROUTER.post('/task', (req, res, next) => {
             'description': req.body.description ? req.body.description : 'Not available',
             'projectId': projectId
         }
-        // We will check if requesting project exist or not in our database
-        let data = {}
-        mongo.connect(process.env.DB_HOST ? process.env.DB_HOST : 'mongodb://localhost:27017', (error, dbClient) => {
-            if (error) {
-                dbClient.close()
-                let err = new Error(error)
-                err['status'] = constants.statusCodes.serverError
-                next(err)
-            }
-            else {
-                let projectList = []
-                const database = dbClient.db('colabnova');
-                let dbCursor = database.collection('projects').find({ 'projectId': projectId });
-                dbCursor.forEach(project => {
-                    projectList.push(project)
-                }, () => {
-                    if (projectList.length > 0) {
-                        let projectData = projectList[0]
-                        let myTasks = projectData['tasks']
-                        myTasks.push(taskData)
-                        database.collection('projects').updateOne({ 'projectId': projectId }, { '$set': { 'tasks': myTasks } }, (error, result) => {
-                            if (error) {
-                                let err = new Error(error)
-                                err['status'] = constants.statusCodes.serverError
-                                next(err)
-                            }
-                            else {
-                                res.status(201).json({
-                                    'message': 'task added',
-                                    'data': result
-                                })
-                            }
+        const identifiers = {
+            'projectId': projectId
+        }
+        utilities.getDocuments('colabnova', 'projects', identifiers)
+            .then(result => {
+                taskData['taskId'] = `TASK_${Math.random().toString(36).slice(2)}-${Math.random().toString(36).slice(2)}-${Math.random().toString(36).slice(2)}`
+                utilities.insertData('colabnova', 'tasks', taskData)
+                    .then(insertionResult => {
+                        res.status(201).json({
+                            'data': insertionResult
                         })
-                    }
-                    else {
-                        let error = new Error('No project found')
-                        error['status'] = constants.statusCodes.unAuthorized
-                        next(error)
-                    }
+                    })
+                    .catch(err => {
+                        res.status(constants.statusCodes.serverError).json({
+                            'data': err
+                        })
+                    })
+            })
+            .catch(error => {
+                res.status(404).json({
+                    'message': 'failed',
+                    'data': error
                 })
-            }
-        })
+            })
     }
     else {
         let error = new Error('No data or projectId found in body')
@@ -247,7 +179,7 @@ ROUTER.delete('/', (req, res, next) => {
             }
             else {
                 const database = dbClient.db('colabnova')
-                database.collection('projects').deleteOne({ 'projectId': projectId }, (error, result) => {
+                database.collection('projects').deleteMany({ 'projectId': projectId }, (error, result) => {
                     if (error || (result.deletedCount == 0)) {
                         let err = new Error(error ? error : 'error while deleting')
                         err['status'] = constants.statusCodes.serverError
@@ -271,5 +203,6 @@ ROUTER.delete('/', (req, res, next) => {
         next(error)
     }
 })
+
 
 module.exports = ROUTER
--- apis/routes/Utilities/index.js
+++ apis/routes/Utilities/index.js
@@ -1,44 +1,68 @@
 const mongo = require('mongodb').MongoClient
 
 module.exports = {
-    getData: function (databaseName, collectionName, keyValue) {
-        if (collectionName && keyValue) {
-            let data = {}
-            mongo.connect(process.env.DB_HOST ? process.env.DB_HOST : 'mongodb://localhost:27017', (error, dbClient) => {
-                if (error) {
-                    dbClient.close()
-                    data['status'] = 'failed'
-                    data['message'] = error
-                    return data
-                }
-                else {
-                    let projectData = []
-                    const database = dbClient.db(databaseName);
-                    let dbCursor = database.collection(collectionName).find(keyValue);
-                    dbCursor.forEach(project => {
-                        projectData.push(project)
-                    }, () => {
-                        if (projectData > 0) {
+    getDocuments: function (databaseName, collectionName, keyValue) {
+        return new Promise((resolve, reject) => {
+            if (collectionName && databaseName) {
+                let data = {}
+                mongo.connect(process.env.DB_HOST ? process.env.DB_HOST : 'mongodb://localhost:27017', (error, dbClient) => {
+                    if (error) {
+                        dbClient.close()
+                        data['data'] = error
+                        // return data
+                        reject(data)
+                    }
+                    else {
+                        let documents = []
+                        const database = dbClient.db(databaseName);
+                        let dbCursor = database.collection(collectionName).find(keyValue);
+                        dbCursor.forEach(document => {
+                            documents.push(document)
+                        }, () => {
                             dbClient.close()
-                            data['status'] = 'success'
-                            data['data'] = projectData
-                            return data
-                        }
-                        else {
-                            dbClient.close()
-                            data['status'] = 'failed'
-                            data['message'] = 'no data found'
-                            return data
-                        }
-                    })
-                }
-            })
-        }
-        else {
-            let error = {}
-            error['status'] = 'failed'
-            error['message'] = 'No collection or identifiers given'
-            return error
-        }
+                            resolve(documents)
+                        })
+                    }
+                })
+            }
+            else {
+                reject({
+                    'data': 'No collection or identifiers given'
+                })
+            }
+        })
+    },
+    insertData: function (databaseName, collectionName, dataToInsert) {
+        return new Promise((resolve, reject) => {
+            if (dataToInsert) {
+                mongo.connect('mongodb://localhost:27017', (error, dbClient) => {
+                    if (error) {
+                        reject({
+                            'data': error
+                        })
+                    }
+                    else {
+                        const database = dbClient.db(databaseName)
+                        database.collection(collectionName).insertOne(dataToInsert, (err, result) => {
+                            if (err) {
+                                reject({
+                                    'data': err
+                                })
+                            }
+                            else {
+                                resolve({
+                                    'data': 'inserted'
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+            else {
+                reject({
+                    'data': 'No data found'
+                })
+            }
+        })
     }
 }
